90d0903baacfedd55cb232c6cbfc0526d3a8e893
buttilloa <anthony.buttillo10@icloud.com>
2015-03-03 10:05:49 -0500

    Finished Part 1

--- Song.java
+++ Song.java
@@ -1,15 +1,40 @@
-public class Song {
+import java.util.Comparator;
+public class Song implements Comparable<Song> {
   
   public String artist;
   public String name;
+  public int year;
+  public double score;
+  public String notes;
   // More fields here
   
   // Default constructor.. you'll need to modify this
-  public Song(String artist, String name) { 
+   public Song(String artist, String name,int year, double score, String notes) { 
     this.artist = artist;
     this.name = name;
+    this.year = year;
+    this.score = score;
+    this.notes = notes;
   }
-  
-  /* ADD YOUR CODE HERE */
+   public String getName()
+   {
+   return this.name;
+   }
+   public int compareTo(Song compareSong) {
+ 
+  return 0;
+ 
+ }
+  public static Comparator<Song> SongNameComparator = new Comparator<Song>() {
+ 
+     public int compare(Song Song1, Song Song2) {
+ 
+       String SongName1 = Song1.getName().toUpperCase();
+       String SongName2 = Song2.getName().toUpperCase();
+ 
+       return SongName1.compareTo(SongName2);
+     }
+       
+     };
   
 }
